8a4e5fc82658e7913e2d7876703a6711da4f2c6a
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-21 15:26:48 +0200

    Added another action that only applies to one animal

--- practiceoop.js
+++ practiceoop.js
@@ -3,13 +3,18 @@ function testOOP(){
 let protoAni = {
     actiAni(){
         document.getElementById("output").innerHTML += (`The ${this.animal} says ${this.sound} <br />`);
+    },
+    eatAni(){
+        document.getElementById("output2").innerHTML += (`The ${this.animal} eats ${this.prey} <br />`);
     }
 }
 
 let shepherdDog = Object.create(protoAni);
 shepherdDog.animal = "Shepherd Dog";
 shepherdDog.sound = "WOOF WOOF!";
+shepherdDog.prey="Tiny Bunny";
 shepherdDog.actiAni();
+shepherdDog.eatAni();
 
 let tinyBunny = Object.create(protoAni);
 tinyBunny.animal = "Tiny bunny";
