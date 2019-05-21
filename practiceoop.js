ddfc815a8b4dafe0f7ccddee05800b8c19717d74
drohnwynandrt <dw.ramdattewari@gmail.com>
2019-05-21 15:35:58 +0200

    Made each story go to their own respective div

--- practiceoop.js
+++ practiceoop.js
@@ -2,14 +2,15 @@ function testOOP(){
 
 let protoAni = {
     actiAni(){
-        document.getElementById("output").innerHTML += (`The ${this.animal} says ${this.sound} <br />`);
+        document.getElementById(this.id).innerHTML += (`The ${this.animal} says ${this.sound} <br />`);
     },
     eatAni(){
-        document.getElementById("output2").innerHTML += (`The ${this.animal} eats ${this.prey} <br />`);
+        document.getElementById(this.id).innerHTML += (`The ${this.animal} eats ${this.prey} <br />`);
     }
 }
 
 let shepherdDog = Object.create(protoAni);
+shepherdDog.id="shepherdDog";
 shepherdDog.animal = "Shepherd Dog";
 shepherdDog.sound = "WOOF WOOF!";
 shepherdDog.prey="Tiny Bunny";
@@ -17,6 +18,7 @@ shepherdDog.actiAni();
 shepherdDog.eatAni();
 
 let tinyBunny = Object.create(protoAni);
+tinyBunny.id="tinyBunny";
 tinyBunny.animal = "Tiny bunny";
 tinyBunny.sound="gimme carrots!";
 tinyBunny.actiAni();
